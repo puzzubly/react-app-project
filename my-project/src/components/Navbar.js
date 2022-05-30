92eeb253f596d03f72ac73233371a2fbcf9fcd7f
neetnet200 <anita.desanmiguel@gmail.com>
2022-05-30 14:55:20 +1000

    added Link

--- my-project/src/components/Navbar.js
+++ my-project/src/components/Navbar.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
-import Logo from './Images/Logo.png'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import './Navbar.css'
+import Logo from "./Images/Logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+
   return (
     <div className="header">
       <div className="navbar">
         <a href="/" className="logo">
-            <img src={Logo} alt='logo'/>
+          <img src={Logo} alt="logo" />
         </a>
         <div className="hamburger" onClick={handleClick}>
-          {click ? (<FaTimes size={32}/>) : (<FaBars size={28}/>)}
+          {click ? <FaTimes size={32} /> : <FaBars size={28} />}
         </div>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-items">
-                <a href='/'>Home</a>
-            </li>
-            <li className="nav-items">
-                <a href='/'>About</a>
-            </li>
-            <li className="nav-items">
-                <a href='/'>Contact</a>
-            </li>
+          <li className="nav-items">
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link to="/about" onClick={closeMenu}>
+              About
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link to="/contact" onClick={closeMenu}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
